90da5d51653c8debf0e2ee0899c481ed83385359
Andrew Churilo <andrewchurilominsk@gmail.com>
2022-08-09 17:15:14 +0300

    Fix "video activity does't appear"
    Problem:
    1. Open video
    2. Collapse
    3. Open Dicti via Launcher
    4. Try open video
    You will not see video activity

--- application/vlc-android/src/org/videolan/vlc/media/PlaylistManager.kt
+++ application/vlc-android/src/org/videolan/vlc/media/PlaylistManager.kt
@@ -370,7 +370,6 @@ open class PlaylistManager(val service: PlaybackService) : MediaWrapperList.Even
         val mw = mediaList.getMedia(index) ?: return
         val isVideoPlaying = mw.type == MediaWrapper.TYPE_VIDEO && player.isVideoPlaying()
         if (!videoBackground && isVideoPlaying) mw.addFlags(MediaWrapper.MEDIA_VIDEO)
-        if (videoBackground) mw.addFlags(MediaWrapper.MEDIA_FORCE_AUDIO)
         if (isBenchmark) mw.addFlags(MediaWrapper.MEDIA_BENCHMARK)
         parsed = false
         player.switchToVideo = false
